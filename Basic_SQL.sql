3172066d3e9c88ac18528db9184595ac614b636d
Dhruv Sharma <dhruv061@hotmail.com>
2020-04-17 20:00:07 +0530

    Add files via upload

--- Basic_SQL.sql
+++ Basic_SQL.sql
@@ -155,11 +155,23 @@ ORDER BY occurred_at DESC;
 
 -- Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. 
 -- Only include the id field in the resulting table.
-
+SELECT id 
+from orders 
+WHERE gloss_qty > 4000 OR poster_qty > 4000;
 
 -- Write a query that returns a list of orders where the standard_qty is zero and 
 -- either the gloss_qty or poster_qty is over 1000.
-
+SELECT * 
+FROM orders 
+WHERE standard_qty = 0 AND (gloss_qty >1000 OR poster_qty >1000);
 
 -- Find all the company names that start with a 'C' or 'W', and 
 -- the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+           AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+           AND primary_poc NOT LIKE '%eana%');
+
+
+-- BASIC SQL ENDS HERE! -- DXT
