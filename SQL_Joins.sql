4f14a19d9e781de5b726f2ea5b06901a96249acf
Dhruv Sharma <dhruv061@hotmail.com>
2020-04-20 14:06:15 +0530

    Add files via upload

--- SQL_Joins.sql
+++ SQL_Joins.sql
@@ -99,3 +99,72 @@ ON o.account_id = a.id;
 -- JOIN statements allow us to pull data from multiple tables in a SQL database.
 
 -- You can use all of the commands we saw in the first lesson along with JOIN statements.
+
+
+-- Provide a table that provides the region for each sales_rep along with 
+-- their associated accounts. This time only for the Midwest region. 
+-- Your final table should include three columns: the region name, 
+-- the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) 
+-- according to account name.
+SELECT r.name r_name, s_r.name s_r_name, a.name a_name 
+FROM sales_reps s_r 
+JOIN region r 
+ON s_r.region_id = r.id AND r.name = 'Midwest' 
+JOIN accounts a ON a.sales_rep_id = s_r.id;
+
+
+-- Provide a table that provides the region for each sales_rep along 
+-- with their associated accounts. This time only for accounts where 
+-- the sales rep has a first name starting with S and in the Midwest region. 
+-- Your final table should include three columns: the region name, the sales rep name, 
+-- and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name r_name, s_r.name s_r_name, a.name a_name 
+FROM sales_reps s_r 
+JOIN region r 
+ON s_r.region_id = r.id AND r.name = 'Midwest' AND s_r.name LIKE 'S%' 
+JOIN accounts a 
+ON a.sales_rep_id = s_r.id 
+ORDER BY a_name;
+
+
+-- Provide a table that provides the region for each sales_rep along 
+-- with their associated accounts. This time only for accounts
+-- where the sales rep has a last name starting with K and in the Midwest region. 
+-- Your final table should include three columns: the region name, the sales rep name, 
+-- and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+
+
+-- Provide the name for each region for every order, as well as the account name 
+-- and the unit price they paid (total_amt_usd/total) for the order. However, 
+-- you should only provide the results if the standard order quantity exceeds 100. 
+-- Your final table should have 3 columns: region name, account name, and unit price. 
+-- In order to avoid a division by zero error, adding .01 to the denominator here is 
+-- helpful total_amt_usd/(total+0.01).
+
+
+-- Provide the name for each region for every order, as well as the account name and 
+-- the unit price they paid (total_amt_usd/total) for the order. However, 
+-- you should only provide the results if the standard order quantity exceeds 100 
+-- and the poster order quantity exceeds 50. Your final table should have 3 columns: 
+-- region name, account name, and unit price. Sort for the smallest unit price first. 
+-- In order to avoid a division by zero error, adding .01 to the denominator here is 
+-- helpful (total_amt_usd/(total+0.01).
+
+
+-- Provide the name for each region for every order, as well as the account name 
+-- and the unit price they paid (total_amt_usd/total) for the order. However, you 
+-- should only provide the results if the standard order quantity exceeds 100 and 
+-- the poster order quantity exceeds 50. Your final table should have 3 columns: region 
+-- name, account name, and unit price. Sort for the largest unit price first. In 
+-- order to avoid a division by zero error, adding .01 to the denominator here is 
+-- helpful (total_amt_usd/(total+0.01).
+
+
+-- What are the different channels used by account id 1001? Your final table should 
+-- have only 2 columns: account name and the different channels. You can try 
+-- SELECT DISTINCT to narrow down the results to only the unique values.
+
+
+-- Find all the orders that occurred in 2015. Your final table should have 4 columns: 
+-- occurred_at, account name, order total, and order total_amt_usd.
+
